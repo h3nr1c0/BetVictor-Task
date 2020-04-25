618cdb7850ca2d9ea045e9b86cb07a4621c774d0
h3nr1c0 <vitalos@gmail.com>
2020-04-25 16:58:02 +0200

    delete unrelevant / unused

--- src/routes/sports.route.js
+++ src/routes/sports.route.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { get_languages } = require('../utils')
-const { getSportsByLanguage, getEventsBySportID } = require('../models/sports.model')
+const { getSportsByLanguage } = require('../models/sports.model')
 
 router
   // GET sports
   .get('/', async (req, res) => {
-    const languages = get_languages(req.query.lang, true);
     // console.log(`GET sporst by languages: ${languages}`)
     getSportsByLanguage(req.query.lang)
     .then(sports => {
